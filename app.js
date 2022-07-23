0f43f9e581c9afc421f49b918ba4c4a57c0862b6
Zhile Lin <zhlin04@gmail.com>
2022-07-23 15:38:41 -0700

    finished set up

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+const express = require("express");
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-const express = require("express");
 const app = express();
-
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>res.send("Hello World"));
 
